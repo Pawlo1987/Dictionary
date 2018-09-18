47b3afae1ebdae67f6fbcebc1024aa89b2bc777a
Pavel Dolliner <paveldolar13@gmail.com>
2018-09-18 12:56:37 +0300

    initial project

---
+++ app/src/main/java/com/example/user/dictionary/AddNewWordActivity.java
@@ -0,0 +1,16 @@
+package com.example.user.dictionary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AddNewWordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_word);
+    }
+
+}
---
+++ app/src/main/java/com/example/user/dictionary/ViewDictionaryActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.dictionary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewDictionaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_dictionary);
+    }
+}
