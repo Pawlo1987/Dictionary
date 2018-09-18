6c59581c22a0d3cb719b42ea2ef461d0c397dd39
Pavel Dolliner <paveldolar13@gmail.com>
2018-09-18 13:46:57 +0300

    corrected the 3-rd method intermittently

--- app/src/main/java/com/example/user/dictionary/ChooseCoupleActivity.java
+++ app/src/main/java/com/example/user/dictionary/ChooseCoupleActivity.java
@@ -22,6 +22,7 @@ public class ChooseCoupleActivity extends AppCompatActivity {
     FileUtilities FileUtilities;
     List<String> listCursorNum; // коллекция id слов для изучения
     List<Word> listWords; // коллекция слов для изучения
+    List<Word> list5WordsOnScreen; // коллекция 5 слов для изучения
     Button btnTr1ChCoAc;
     Button btnTr2ChCoAc;
     Button btnTr3ChCoAc;
@@ -33,7 +34,6 @@ public class ChooseCoupleActivity extends AppCompatActivity {
     Button btnHe4ChCoAc;
     Button btnHe5ChCoAc;
     List<Button> buttonsList;//коллекция кнопок
-    int selectPos = 0;  //выбранная позиция
     int wordsCount = 10;
     int progressTime = 0;
     int progressIter;
@@ -45,7 +45,7 @@ public class ChooseCoupleActivity extends AppCompatActivity {
     Boolean[] flagPressBtnHe = {false,false,false,false,false}; //массив флагов нажатых кнопок
     boolean flagAnyBtnPressTr = false; //проверка нажата какая-нибудь кнопка
     boolean flagAnyBtnPressHe = false; //проверка нажата какая-нибудь кнопка
-    CountDownTimer countDownTimer;
+    int countRemainingWords; //счетчик оставшихся неотгаданых слов
     ProgressBar pbChCoAc;
 
     private Cursor cursor;
@@ -73,6 +73,7 @@ public class ChooseCoupleActivity extends AppCompatActivity {
         wordTr = new ArrayList<>();
         wordHe = new ArrayList<>();
         listWords  = new ArrayList<>();
+        list5WordsOnScreen  = new ArrayList<>();
         pbChCoAc = findViewById(R.id.pbChCoAc);
         buttonsList = new ArrayList<>();
         btnTr1ChCoAc = findViewById(R.id.btnTr1ChCoAc);
@@ -183,13 +184,16 @@ public class ChooseCoupleActivity extends AppCompatActivity {
     //проверка совпадения нажатой пары клавиш
     private void checkPressBtn() {
         //сложная проверка))
-        //по русскому слову находим порядковый номер в коллекции
+        //по русскому слову паралельно и по ивритовскому слово
+        //находим порядковый номер в коллекции
+        int indexHe = wordHe.indexOf(selectWordHe);
         int indexTr = wordTr.indexOf(selectWordTr);
 
-        //и проверяем совподает ли слово на кнопке со словом в коллекцыи
-        // ивритовских слов
+        //и проверяем совподает ли слово на кнопке со словом в коллекции
+        // ивритовских слов и паралельно русских слов
         String strHe  = wordHe.get(indexTr);
-        if(selectWordHe.equals(strHe)){
+        String strTr  = wordTr.get(indexHe);
+        if((selectWordHe.equals(strHe))||(selectWordTr.equals(strTr))){
             //снимаем флаги нажатых клавиш
             flagAnyBtnPressHe = false;
             flagAnyBtnPressTr = false;
@@ -214,48 +218,78 @@ public class ChooseCoupleActivity extends AppCompatActivity {
                     }//if
                 }////if-else
             }//for
+
+            //проверка сколько слов осталось угадать
+            //если все угаданы формируем экран заново
+            if(countRemainingWords > 0) countRemainingWords--;
+            else{
+                //удаляем из коллекции listWords отгаданые слова
+                for (int i = 0; i < 5; i++) {
+                    listWords.remove(list5WordsOnScreen.get(i));
+                }//for
+                //очищяем временую коллекцию для следующей порции слов
+                list5WordsOnScreen.clear();
+                startLearnWord();
+            }//if-else
         }//if
     }//checkPressBtn
 
     //запуск начала изучения
+    //формирование экранна со словами для изучения
     private void startLearnWord() {
-        List<Integer> listNumForBtn = new ArrayList<>();
-        listNumForBtn.add(selectPos);
-        for (int i = 0; i < 4; i++) {
-            int k;
-            while(true) {
-                k = Utils.getRandom(0, wordsCount-1);
-                if((k>0)&&(k<wordsCount-1)&&(!listNumForBtn.contains(k))) break;
-            }//while
-            listNumForBtn.add(k);
+        //устанавливаем счетчик оставшихся значений
+        countRemainingWords = 4;
+
+        //5 id-ков из коллекции значений коллекция listWords
+        // для установки в название кнопок
+        List<Integer> listIdNumForBtn = new ArrayList<>();
+
+        //проверяем сколько слов осталось
+        //в коллекции listWords для изучения
+        //и устанавлеваем их id в коллекцию listIdNumForBtn
+        int n = listWords.size();
+        if(n >= 5) n=5; else n--;
+        for (int i = 0; i < n; i++) {
+            //заполняем временую коллекцию id слов для вывода на экран
+            listIdNumForBtn.add(i);
+
+            //заполняем временую коллекцию слов для вывода на экран
+            list5WordsOnScreen.add(listWords.get(i));
         }//for
-
-        //перемешать коллекцию переводов для вывода названия кнопок
-        Collections.shuffle(listNumForBtn);
-
-        //коллекция порядковых номеров для коллекции заполняемых кнопки
-        List<Integer> numIdListForBtn = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            numIdListForBtn.add(i);
-        }//fori
 
         //устанавливаем нормальный цвет кнопок
         for (int i = 0; i < 10; i++) {
             buttonsList.get(i).setBackgroundColor(context.getResources().getColor(R.color.colorButtonNormal));
         }//for
+
         //устанавливаем слова на кнопки
         //перемешать коллекцию
-        Collections.shuffle(numIdListForBtn);
+        Collections.shuffle(listIdNumForBtn);
+
         int j;
         for (int i = 0; i < 10; i++) {
-            if(i==5) Collections.shuffle(numIdListForBtn);
+            //делаем кнопки активными,
+            // так как они становятся неактивные
+            // после первого отгаданого окна
+            buttonsList.get(i).setEnabled(true);
+            //если i==5, первым делом перемешаем
+            //коллекцию порядковых номеров,
+            //чтоб порядок слов в первом столбике отличались
+            //от порядка слов во втором столбике.
+            //перемешиваем пару раз чтоб эффективнее
+            if(i==5) {
+                Collections.shuffle(listIdNumForBtn);
+                Collections.shuffle(listIdNumForBtn);
+            }
+            //i:0-4 устанавливаем слова в первом столбике
+            //i:5-9 устанавливаем слова во втором столбике
             if(i<=4){
                 j=i;
-                buttonsList.get(i).setText(listWords.get(listNumForBtn.get(numIdListForBtn.get(j))).getStrRus());
+                buttonsList.get(i).setText(list5WordsOnScreen.get(listIdNumForBtn.get(j)).getStrRus());
             }else{
                 j = i-5;
-                buttonsList.get(i).setText(listWords.get(listNumForBtn.get(numIdListForBtn.get(j))).getStrHeb());
-            }
+                buttonsList.get(i).setText(list5WordsOnScreen.get(listIdNumForBtn.get(j)).getStrHeb());
+            }//if-else
         }//for
     }//startLearnWord
 
@@ -276,15 +310,16 @@ public class ChooseCoupleActivity extends AppCompatActivity {
             word.setStrRus(cursor.getString(1));     //слово на русском
             word.setStrHeb(cursor.getString(2));     //слово на иврите
             word.setStrTrans(cursor.getString(3));   //транскрпция слова на иврите
-            word.setGenRus(cursor.getString(4));     //род слова в русском
-            word.setGenHeb(cursor.getString(5));     //род слова в иврите
-            word.setMeaning(cursor.getString(6));    //значение слова в предложении
-            word.setQuantity(cursor.getString(7));   //множественное или едиственное слово
+//            word.setGenRus(cursor.getString(4));     //род слова в русском
+//            word.setGenHeb(cursor.getString(5));     //род слова в иврите
+//            word.setMeaning(cursor.getString(6));    //значение слова в предложении
+//            word.setQuantity(cursor.getString(7));   //множественное или едиственное слово
 
             //добавляем новое слово в коллекцию
             listWords.add(word);
             word = new Word();
         }//while
-
+        //перемешаем полученную коллекцию
+//        Collections.shuffle(listWords);
     }//createWordList
 }//ChooseCoupleActivity
