0f92d63b608744c33ea96bceac8242c8f2d6c847
Pavel Dolliner <paveldolar13@gmail.com>
2018-09-18 13:38:20 +0300

    Setting text to elements for learning

--- app/src/main/java/com/example/user/dictionary/ChooseTranslationActivity.java
+++ app/src/main/java/com/example/user/dictionary/ChooseTranslationActivity.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +17,12 @@ public class ChooseTranslationActivity extends AppCompatActivity {
     FileUtilities FileUtilities;
     List<String> listIdLearnWords; // коллекция id слов для изучения
     List<Word> listWords; // коллекция слов для изучения
+    TextView tvWordChTrAc;
+    Button btnTr1ChTrAc;
+    Button btnTr2ChTrAc;
+    Button btnTr3ChTrAc;
+    Button btnTr4ChTrAc;
+    Button btnTr5ChTrAc;
 
     private Cursor cursor;
 
@@ -38,12 +47,54 @@ public class ChooseTranslationActivity extends AppCompatActivity {
         dbUtilities.open();
         listIdLearnWords = new ArrayList<>();
         listWords  = new ArrayList<>();
+        tvWordChTrAc = findViewById(R.id.tvWordChTrAc);
+        btnTr1ChTrAc = findViewById(R.id.btnTr1ChTrAc);
+        btnTr2ChTrAc = findViewById(R.id.btnTr2ChTrAc);
+        btnTr3ChTrAc = findViewById(R.id.btnTr3ChTrAc);
+        btnTr4ChTrAc = findViewById(R.id.btnTr4ChTrAc);
+        btnTr5ChTrAc = findViewById(R.id.btnTr5ChTrAc);
         cursor = dbUtilities.getDb().rawQuery(mainQuery, null);
         listIdLearnWords.addAll(getIntent().getStringArrayListExtra("idList"));
         createWordList();
-       if(true);
+//        for (int i = 0; i < 20; i++) {
+            startLearnWord(Utils.getRandom(0, 19));
+//        }
     }//onCreate
 
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnTr1ChTrAc:
+            case R.id.btnTr2ChTrAc:
+            case R.id.btnTr3ChTrAc:
+            case R.id.btnTr4ChTrAc:
+            case R.id.btnTr5ChTrAc:
+                btnSelect();
+                break;
+
+        }//switch
+
+    }//onClick
+
+    private void btnSelect() {
+    }
+
+    //запуск начала изучения
+    private void startLearnWord(int selectPos) {
+        tvWordChTrAc.setText(listWords.get(selectPos).getStrHeb().toString());
+        List<Integer> listNumForBtn = new ArrayList<>();
+        listNumForBtn.add(selectPos);
+        for (int i = 0; i < 4; i++) {
+            int k = Utils.getRandom(0, 19);
+            listNumForBtn.add(k!=selectPos?k:k+1);
+        }
+        btnTr1ChTrAc.setText(listWords.get(listNumForBtn.get(0)).getStrRus().toString());
+        btnTr2ChTrAc.setText(listWords.get(listNumForBtn.get(1)).getStrRus().toString());
+        btnTr3ChTrAc.setText(listWords.get(listNumForBtn.get(2)).getStrRus().toString());
+        btnTr4ChTrAc.setText(listWords.get(listNumForBtn.get(3)).getStrRus().toString());
+        btnTr5ChTrAc.setText(listWords.get(listNumForBtn.get(4)).getStrRus().toString());
+    }//startLearnWord
+
+    //создаем коллекцию объектов слов для изучения
     private void createWordList() {
         Word word = new Word();
         for (int i = 0; i < 20; i++) {
